4413772181724af3b06c31ed0f8390dc7a20f568
James Kuyt <jkuyt39@gmail.com>
2023-04-01 23:13:38 +0000

    meow

---
+++ app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDA0X9OkthCMMvhxXIwUvVjeqzjNNT8b_k",
+    authDomain: "newvideo2212.firebaseapp.com",
+    databaseURL: "https://newvideo2212-default-rtdb.firebaseio.com",
+    projectId: "newvideo2212",
+    storageBucket: "newvideo2212.appspot.com",
+    messagingSenderId: "283500325791",
+    appId: "1:283500325791:web:4715dfd7a607b511832baa"
+  };
+  firebase.initializeApp(firebaseConfig);
+  
