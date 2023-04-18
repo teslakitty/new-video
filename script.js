ac8fb3483db1747ca5708bbc6de52a063709aba6
James Kuyt <jkuyt39@gmail.com>
2023-04-18 23:26:16 +0000

    meow

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ import 'firebase/database';
 import Twilio from 'twilio-video'; // Import Twilio here
 
 const firebaseConfig = {
-  // your Firebase config here
+  apiKey: "AIzaSyDA0X9OkthCMMvhxXIwUvVjeqzjNNT8b_k",
+  authDomain: "newvideo2212.firebaseapp.com",
+  databaseURL: "https://newvideo2212-default-rtdb.firebaseio.com",
+  projectId: "newvideo2212",
+  storageBucket: "newvideo2212.appspot.com",
+  messagingSenderId: "283500325791",
+  appId: "1:283500325791:web:4715dfd7a607b511832baa"
 };
-
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
